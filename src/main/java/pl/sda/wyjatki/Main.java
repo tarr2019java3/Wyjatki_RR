00a2bdd316b08aabe7f07fbf4173aed17b34c4ce
Rafal <rostkos@gmail.com>
2020-03-07 14:29:46 +0100

    Zadanie

--- src/main/java/pl/sda/wyjatki/Main.java
+++ src/main/java/pl/sda/wyjatki/Main.java
@@ -1,8 +1,24 @@
 package pl.sda.wyjatki;
 
+import org.w3c.dom.ls.LSOutput;
+import pl.sda.wyjatki.exception.EmailException;
+
+import java.nio.channels.ScatteringByteChannel;
+import java.util.EmptyStackException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
+    public static void main(String[] args)  {
+
+        User user = new User();
+        user.setLogin("login");
+       try {
+           user.setEmail("adres");
+       } catch (EmailException e) {
+          e.printStackTrace();
+           }
+
+           }
+             }
+
